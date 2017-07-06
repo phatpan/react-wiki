a914557449726e14bfe72b91bf2e667e3c9936b8
phatcharaphan <phat.pan@gmail.com>
2017-07-06 12:40:31 +0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  devtool: 'eval',
+  entry: './index.js',
+  output: {
+    publicPath: '/static/',
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loaders: [
+      'babel-loader'
+    ]
+  },
+  {
+    // สำหรับไฟล์นามสกุล css ให้ใช้ Loader สองตัวคือ css-loader และ style-loader
+    test: /\.css$/,
+    loaders: [
+      'style-loader',
+      'css-loader'
+    ]
+  }, {
+    // ใช้ Loader สามตัวสำหรับ scss
+    test: /\.scss$/,
+    exclude: /node_modules/,
+    loaders: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        query: {
+          sourceMap: true,
+          module: true,
+          localIdentName: '[local]___[hash:base64:5]'
+        }
+      },
+      {
+        loader: 'sass-loader',
+        query: {
+          outputStyle: 'expanded',
+          sourceMap: true
+        }
+      }
+    ]
+  }]
+},
+plugins: [
+  new webpack.LoaderOptionsPlugin({ options: { postcss: [ autoprefixer ] } })
+],
+};
