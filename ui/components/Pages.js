46981e754accbb60e3fc12c05a8181b5bf507e5d
phatcharaphan <phat.pan@gmail.com>
2017-07-07 15:58:29 +0700

    modify endpoint

--- ui/components/Pages.js
+++ ui/components/Pages.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import fetch from 'isomorphic-fetch'
 import SharedPages from './SharedPages'
+import { PAGES_ENDPOINT } from '../constants/endpoints'
 
 class Pages extends Component {
   state = {
@@ -8,7 +9,7 @@ class Pages extends Component {
   }
 
   componentDidMount(){
-    fetch('/api/v1/pages')
+    fetch(PAGES_ENDPOINT)
     .then((response)=> response.json())
     .then((pages) => {
       this.setState({pages})
--- ui/components/PopularPages.js
+++ ui/components/PopularPages.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import fetch from 'isomorphic-fetch'
 import SharedPages from './SharedPages'
+import { PAGES_POPULAR_ENDPOINT } from '../constants/endpoints'
 
 export default class PopularPages extends Component{
     state = {
         pages: []
     }
     componentDidMount(){
-        fetch('/api/v1/pages?popular=true')
+        fetch(PAGES_POPULAR_ENDPOINT)
         .then((response)=>response.json())
         .then((pages)=>this.setState({pages}))
     }
