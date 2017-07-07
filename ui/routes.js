a28c14f2c3d12cbcb2dad142335539ffc294019b
phatcharaphan <phat.pan@gmail.com>
2017-07-07 16:43:48 +0700

    add route view pages

--- ui/routes.js
+++ ui/routes.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import App from './App/App'
 import Home from './components/Home'
-import Pages from './containers/Pages/list'
+import Pages from './containers/Pages/List'
+import ViewPage from './containers/Pages/View'
 
 export default () => {
   return (
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={Home} />
-        <route path='pages' component={Pages} />
+        <route path='pages'>
+          <IndexRoute component={Pages}/>
+          <route path=':id' component={ViewPage}/>
+        </route>
       </Route>
     </Router>
   )
