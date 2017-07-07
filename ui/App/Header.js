1835692fab78e1cc78df168eab2382dd7b086814
phatcharaphan <phat.pan@gmail.com>
2017-07-07 14:08:59 +0700

    change a tag to Link from react-router

--- ui/App/Header.js
+++ ui/App/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Link from 'react-router'
+import { Link } from 'react-router'
 import styles from './Header.scss'
 
 export default class Header extends Component {
