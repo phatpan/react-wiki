2a09f70b3da75c7072bdb1e9c93ce818f0e884b0
phatcharaphan <phat.pan@gmail.com>
2017-07-07 16:19:27 +0700

    refactor class component to function component

--- ui/components/SharedPages.js
+++ ui/components/SharedPages.js
@@ -1,35 +1,31 @@
 import React, {Component, PropTypes} from 'react'
 import Page from './Page'
-export default class SharedPages extends Component{
-    static PropTypes = {
-        pages : PropTypes.array.isRequired,
-        onReloadPages: PropTypes.func.isRequired
-    }
-    render(){
-        const {pages, onReloadPages} = this.props
-        return (
-            <div>
-                <button className='button'
-                        onClick={() => onReloadPages()}>
-                        Reload Pages
-                </button><hr/>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            pages.map((page) => (
-                            <Page key={page.id} id={page.id} title={page.title} />
-                            ))
-                        }
-                        </tbody>
-                </table>
-            </div>
-        )
-    }
+const SharedPages = ({pages, onReloadPages}) => (
+    <div>
+        <button className='button'
+                onClick={() => onReloadPages()}>
+                Reload Pages
+        </button><hr/>
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    pages.map((page) => (
+                    <Page key={page.id} id={page.id} title={page.title} />
+                    ))
+                }
+                </tbody>
+        </table>
+    </div>
+)
+SharedPages.PropTypes = {
+    pages : PropTypes.array.isRequired,
+    onReloadPages: PropTypes.func.isRequired
 }
+export default SharedPages
