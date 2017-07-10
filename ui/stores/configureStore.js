6864cc54b9f66288ad6fcc6fd6d2963b302b8300
phatcharaphan <phat.pan@gmail.com>
2017-07-11 00:01:44 +0700

    verify promise before use dispatch

--- ui/stores/configureStore.js
+++ ui/stores/configureStore.js
@@ -1,9 +1,19 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers'
 
+const promise =(store)=> {
+  const next = store.dispatch
+  return (action)=>{
+    if(typeof action.then === 'function')
+      return action.then(next)
+    
+    return next(action)
+  }
+}
+
 export default () => {
   const store = createStore(rootReducer)
-  
+  store.dispatch = promise(store)
   if (module.hot) {
     System.import('../reducers').then(nextRootReducer =>
       store.replaceReducer(nextRootReducer.default)
